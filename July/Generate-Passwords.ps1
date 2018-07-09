dcebf18de2af414d79880a7d18f2d633baf966e3
spixo <spixo@ckeffect.com>
2018-07-09 18:57:04 +0000

    Added July's examples

---
+++ July/Generate-Passwords.ps1
@@ -0,0 +1,36 @@
+﻿
+
+<#=====================================================================
+
+        Generate random 14-18 character password
+
+======================================================================#>
+
+
+function Get-RandomPassword
+{
+    $asci = [char[]]([char]33..[char]95) + ([char[]]([char]97..[char]126))
+    $RandomASCI = (1..$(Get-Random -Min 13 -Max 17) | % {$asci | get-random}) -join “”
+    $RandomPassword = ($RandomASCI -replace "(?<=^.{$(Get-Random -Min 3 -Max 11)}).","$(Get-Random -Min 0 -Max 99)")
+
+    return $RandomPassword
+}
+
+
+
+
+<#=====================================================================
+
+        Generate semi-unique password via approved phrase list
+
+======================================================================#>
+
+
+function Get-PhrasePassword
+{
+    $Words = Get-Content "$PSScriptRoot\Pass-List.txt" 
+    $r = Get-Random -Maximum $Words.Count
+    [string]$PhrasePassword = $Words[$r] + (Get-Random -Minimum 10 -Maximum 999)
+
+    return $PhrasePassword
+}
---
+++ July/Secure-Credentials.ps1
@@ -0,0 +1,61 @@
+﻿
+
+break  ## Prevent Accidental Run of Full Script
+
+
+<#=====================================================================
+
+        Generate 256 Bit AES Encryption Key
+
+======================================================================#>
+
+
+$KeyPath = ".\AES-Encryption.key"
+
+$Key = New-Object Byte[] 32
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+$Key | Out-File $KeyPath
+
+
+<#=====================================================================
+
+        Export Credentials
+
+======================================================================#>
+
+
+$CredsPath = ".\Name-of-File.xml"  ## be sure to update the "Name-of-File.xml" at the end. 
+$KeyPath = ".\AES-Encryption.key"
+
+$Key = Get-Content $KeyPath
+$Creds = Get-Credential
+$Creds = $Creds | Select-Object *
+$creds.Password = $Creds.Password | ConvertFrom-SecureString -Key $Key
+$Creds | Export-Clixml $CredsPath
+
+
+<#=====================================================================
+
+        Import Credentials
+
+======================================================================#>
+
+
+$CredsPath = ".\Name-of-File.xml"  ## be sure to update the "Name-of-File.xml" at the end. 
+$KeyPath = ".\AES-Encryption.key"
+
+$Key = Get-Content $KeyPath
+$Creds = Import-Clixml $CredsPath
+$Creds.Password = $Creds.Password | ConvertTo-SecureString -Key $Key
+
+$AdminCredentials = New-Object -TypeName System.Management.Automation.PSCredential($Creds.UserName,$Creds.Password)
+
+
+<#=====================================================================
+
+        Example Usage After Import
+
+======================================================================#>
+
+
+Restart-Computer -ComputerName "Test-VM-01" -Credential $AdminCredentials -Force  ## Used with the $AdminCredentials variable. 
